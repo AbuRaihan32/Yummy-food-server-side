63988a8e1613323cf980d85f06b81752b7c5373f
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-05-11 18:07:01 +0600

    post a food system added

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ async function run() {
     })
 
 
+    // ! post a food 
+    app.post('/addFood', async(req, res) =>{
+      const newFood = req.body;
+      const result = await foodCollection.insertOne(newFood);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
